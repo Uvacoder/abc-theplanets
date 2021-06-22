c76de02b918da5011a1b4f13a2f0f8bd4f57bc5a
SJellen <sjellen@me.com>
2021-06-22 15:06:01 -0400

    work on planet specific highlight color

--- context/Context.js
+++ context/Context.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState, useCallback} from 'react'
 import data from '../data/data.json'
 
 const Context = React.createContext()
@@ -8,6 +8,7 @@ function ContextProvider({ children }) {
     const [planets, setPlanets] = useState([...data])
     const [currentPlanet, setCurrentPlanet] = useState(data[2])
     const [currentLayer, setCurrentLayer] = useState("overview")
+    
 
     function handleNavClick(arr) {
         setCurrentPlanet(arr)
@@ -27,10 +28,25 @@ function ContextProvider({ children }) {
 
     
 
-    console.log(currentPlanet)
+    function planetSpecificColor() {
+        return currentPlanet === "Mercury" ? "#419EBB" :
+               currentPlanet === "Venus" ? "#EDA249" :
+               currentPlanet === "Earth" ? "#6D2ED5" :
+               currentPlanet === "Mars" ? "#D14C32" :
+               currentPlanet === "Jupiter" ? "#D83A34" :
+               currentPlanet === "Saturn" ? "#CD5120" :
+               currentPlanet === "Uranus" ? "#1EC1A2" :
+               "#2D68F0"
+    }
+
+    
+
+    
+
+    
 
     return (
-        <Context.Provider value={{currentPlanet, setCurrentPlanet, handleNavClick, planets, handleOverviewClick, handleInternalStructureClick, handleSurfaceGeologyClick, currentLayer}}>
+        <Context.Provider value={{currentPlanet, setCurrentPlanet, handleNavClick, planets, handleOverviewClick, handleInternalStructureClick, handleSurfaceGeologyClick, currentLayer, planetSpecificColor}}>
             {children}
         </Context.Provider>
     )
