d617cdacab218e7a5265dde476bb5f4860455933
SJellen <sjellen@me.com>
2021-06-23 09:47:43 -0400

    planet info text switch set

--- components/PlanetInfo.js
+++ components/PlanetInfo.js
@@ -6,15 +6,16 @@ export default function PlanetImage() {
 
     const {currentPlanet, handleOverviewClick, handleInternalStructureClick, handleSurfaceGeologyClick, currentLayer} = useContext(Context)
 
-    
-
-    
+    const planetLayerText = currentLayer === "internal" ? currentPlanet.structure.content : 
+                            currentLayer === "surface" ? currentPlanet.geology.content : currentPlanet.overview.content
 
     return (
         <div className={styles.container}>
             <div className={styles.infoTextContainer}>
                 <h1 className={styles.infoTextContainerH1}>{currentPlanet.name}</h1>
-                <p className={styles.infoTextContainerP}>Much of the Venusian surface appears to have been shaped by volcanic activity. Venus has several times as many volcanoes as Earth, and it has 167 large volcanoes that are over 100 km (60 mi) across. The only volcanic complex of this size on Earth is the Big Island of Hawaii.</p>
+                <p className={styles.infoTextContainerP}>
+                    { planetLayerText }
+                </p>
                 <h3 className={styles.infoTextContainerH3}>Source: <span className={styles.infoTextContainerSpan}>Wikipedia</span><a href="https://en.wikipedia.org/wiki/Venus#Surface_geology"></a></h3>
             </div>
             <div className={styles.buttonContainer}>
@@ -36,8 +37,6 @@ export default function PlanetImage() {
                     
                         <h4><span className={styles.buttonTextSpan}>03 </span>SURFACE GEOLOGY</h4></button>
             </div>
-            
-            
         </div>
     )
 }
