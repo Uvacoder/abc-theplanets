d97b0e68d63c03267e9f50c52a608e562b1b8e92
SJellen <sjellen@me.com>
2021-06-23 14:52:53 -0400

    work on adding link icon for wiki page

--- components/PlanetInfo.js
+++ components/PlanetInfo.js
@@ -16,26 +16,26 @@ export default function PlanetImage() {
                 <p className={styles.infoTextContainerP}>
                     { planetLayerText }
                 </p>
-                <h3 className={styles.infoTextContainerH3}>Source: <span className={styles.infoTextContainerSpan}>Wikipedia</span><a href="https://en.wikipedia.org/wiki/Venus#Surface_geology"></a></h3>
+                <h3 className={styles.infoTextContainerH3}>Source: <span className={styles.infoTextContainerSpan}>Wikipedia</span><a href="https://en.wikipedia.org/wiki/Venus#Surface_geology"><svg className={styles.svg} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm1 15.889v-2.223s-3.78-.114-7 3.333c1.513-6.587 7-7.778 7-7.778v-2.221l5 4.425-5 4.464z"/></svg></a></h3>
             </div>
             <div className={styles.buttonContainer}>
                 <button 
                     className={styles.button} 
                     onClick={handleOverviewClick} 
                     style={{backgroundColor: currentLayer === "overview" ? currentPlanet.highlight : ''}}>
-                        <h4><span className={styles.buttonTextSpan}>01 </span>OVERVIEW</h4></button>
+                        <h4 className={styles.buttonTextH4}><span className={styles.buttonTextSpan}>01 </span>OVERVIEW</h4></button>
                 <button 
                     className={styles.button} 
                     onClick={handleInternalStructureClick}
                     style={{backgroundColor: currentLayer === "internal" ? currentPlanet.highlight : ''}}>
                     
-                        <h4><span className={styles.buttonTextSpan}>02 </span>INTERNAL STRUCTURE</h4></button>
+                        <h4 className={styles.buttonTextH4}><span className={styles.buttonTextSpan}>02 </span>INTERNAL STRUCTURE</h4></button>
                 <button 
                     className={styles.button} 
                     onClick={handleSurfaceGeologyClick}
                     style={{backgroundColor: currentLayer === "surface" ? currentPlanet.highlight : ''}}>
                     
-                        <h4><span className={styles.buttonTextSpan}>03 </span>SURFACE GEOLOGY</h4></button>
+                        <h4 className={styles.buttonTextH4}><span className={styles.buttonTextSpan}>03 </span>SURFACE GEOLOGY</h4></button>
             </div>
         </div>
     )
