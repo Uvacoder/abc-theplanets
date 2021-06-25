c454579e37b80c09d9f27f6148a6f71c8f55efd4
SJellen <sjellen@me.com>
2021-06-25 14:17:59 -0400

    work on mobile nav and planet layout

--- components/HamburgerNav.js
+++ components/HamburgerNav.js
@@ -6,11 +6,28 @@ export default function HamburgerNav() {
 
     const {handleNavClick, planets, currentPlanet, isNavOpen} = useContext(Context)
 
+    const navPlanets = planets.map((planet, index) => (
+        <li key={index} 
+            className={styles.li}
+            ><div className={styles.leftFlex}>
+               <div className={styles.highlight} style={{backgroundColor: planet.highlight}}></div>
+                <div className={styles.planetName}>{planet.name}</div>  
+            </div>
+            <div className={styles.rightFlex}>
+            <svg 
+            className={styles.svgArrow}
+            xmlns="http://www.w3.org/2000/svg" 
+            width="24" height="24" 
+            viewBox="0 0 24 24"><path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z"/></svg>
+            </div>
+        </li>
+    ))
+
     return (
         <div className={styles.container}>
             <nav className={styles.nav} style={{display: isNavOpen ? "" : 'none'}} >
-                <ul >
-
+                <ul className={styles.ul} >
+                    <li>{navPlanets}</li>
                 </ul>
             </nav>
         </div>   
